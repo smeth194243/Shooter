6a41ee6750d5c01cb6b7097d1ba81a15060548a4
smeth194243 <smor7432@740s-pl-106.local>
2017-05-08 12:39:09 -0600

    OMG THIS IS SOOO LEGITTTTT

--- Shooter/Controller/Program.cs
+++ Shooter/Controller/Program.cs
@@ -11,7 +11,7 @@ using UIKit;
 #endif
 #endregion
 
-namespace Shooter
+namespace Shooter.Controller
 {
 #if __IOS__ || __TVOS__
     [Register("AppDelegate")]
--- Shooter/Controller/SteamPunkShooterGame.cs
+++ Shooter/Controller/SteamPunkShooterGame.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input.Touch;
 using Shooter.Model;
 
-namespace Shooter
+namespace Shooter.Controller
 {
 	/// <summary>
 	/// This is the main type for your game.
@@ -29,6 +32,30 @@ namespace Shooter
 
 		// A random number generator
 		Random random;
+
+		Texture2D projectileTexture;
+		List<Projectile> projectiles;
+
+		// The rate of fire of the player laser
+		TimeSpan fireTime;
+		TimeSpan previousFireTime;
+
+		Texture2D explosionTexture;
+		List<Animation> explosions;
+
+		// The sound that is played when a laser is fired
+		SoundEffect laserSound;
+
+		// The sound used when the player or an enemy dies
+		SoundEffect explosionSound;
+
+		// The music played during gameplay
+		Song gameplayMusic;
+
+//Number that holds the player score
+int score;
+// The font used to display UI elements
+SpriteFont font;
 
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
@@ -83,6 +110,15 @@ namespace Shooter
 			// Set a constant player move speed
 			playerMoveSpeed = 8.0f;
 
+			projectiles = new List<Projectile>();
+
+			// Set the laser to fire every quarter second
+			fireTime = TimeSpan.FromSeconds(.15f);
+
+			explosions = new List<Animation>();
+
+			//Set player's score to zero
+score = 0;
 
 			base.Initialize();
 		}
@@ -111,6 +147,21 @@ namespace Shooter
 			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
 
 			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
+			projectileTexture = Content.Load<Texture2D>("Texture/laser");
+			explosionTexture = Content.Load<Texture2D>("Animation/explosion");
+
+			// Load the music
+			gameplayMusic = Content.Load<Song>("Sound/gameMusic");
+
+			// Load the laser and explosion sound effect
+			laserSound = Content.Load<SoundEffect>("Sound/laserFire");
+			explosionSound = Content.Load<SoundEffect>("Sound/explosion");
+
+			// Start the music right away
+			PlayMusic(gameplayMusic);
+
+			// Load the score font
+font = Content.Load<SpriteFont>("Font/gameFont");
 
 			//TODO: use this.Content to load your game content here 
 		}
@@ -145,6 +196,14 @@ namespace Shooter
 			bgLayer2.Update();
 			// Update the enemies
 			UpdateEnemies(gameTime);
+			// Update the collision
+			UpdateCollision();
+
+			// Update the projectiles
+			UpdateProjectiles();
+
+			// Update the explosions
+			UpdateExplosions(gameTime);
 			base.Update(gameTime);
 		}
 
@@ -181,6 +240,38 @@ namespace Shooter
 			// Make sure that the player does not go out of bounds
 			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
 			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+
+			if (currentKeyboardState.IsKeyDown(Keys.Space))
+			{
+				// Fire only every interval we set as the fireTime
+				if (gameTime.TotalGameTime - previousFireTime > fireTime)
+				{
+					// Reset our current time
+					previousFireTime = gameTime.TotalGameTime;
+
+					// Add the projectile, but add it to the front and center of the player
+					AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
+					// Play the laser sound
+					laserSound.Play();
+				}
+			}
+
+			// reset score if player health goes to zero
+if (player.Health <= 0)
+{
+player.Health = 100;
+score = 0;
+}
+
+			// Fire only every interval we set as the fireTime
+			//	if (gameTime.TotalGameTime - previousFireTime > fireTime)
+			//	{
+			//		// Reset our current time
+			//		previousFireTime = gameTime.TotalGameTime;
+			//
+			//		// Add the projectile, but add it to the front and center of the player
+			//		AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
+			//	}
 		}
 
 		/// <summary>
@@ -206,6 +297,23 @@ namespace Shooter
 			{
 				enemies[i].Draw(spriteBatch);
 			}
+
+			// Draw the Projectiles
+			for (int i = 0; i < projectiles.Count; i++)
+			{
+				projectiles[i].Draw(spriteBatch);
+			}
+
+			// Draw the explosions
+			for (int i = 0; i < explosions.Count; i++)
+			{
+				explosions[i].Draw(spriteBatch);
+			}
+
+			// Draw the score
+spriteBatch.DrawString(font, "score: " + score, new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y), Color.White);
+// Draw the player health
+spriteBatch.DrawString(font, "health: " + player.Health, new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + 30), Color.White);
 			// Stop drawing
 			spriteBatch.End();
 			//TODO: Add your drawing code here
@@ -252,10 +360,137 @@ namespace Shooter
 
 				if (enemies[i].Active == false)
 				{
+					// If not active and health <= 0
+					if (enemies[i].Health <= 0)
+					{
+						// Add an explosion
+						AddExplosion(enemies[i].Position);
+						// Play the explosion sound
+						explosionSound.Play();
+						//Add to the player's score
+score += enemies[i].Value;
+					}
 					enemies.RemoveAt(i);
 				}
 
 			}
 		}
+
+		private void UpdateCollision()
+		{
+			// Use the Rectangle's built-in intersect function to 
+			// determine if two objects are overlapping
+			Rectangle rectangle1;
+			Rectangle rectangle2;
+
+			// Only create the rectangle once for the player
+			rectangle1 = new Rectangle((int)player.Position.X,
+			(int)player.Position.Y,
+			player.Width,
+			player.Height);
+
+			// Do the collision between the player and the enemies
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				rectangle2 = new Rectangle((int)enemies[i].Position.X,
+				(int)enemies[i].Position.Y,
+				enemies[i].Width,
+				enemies[i].Height);
+
+				// Determine if the two objects collided with each
+				// other
+				if (rectangle1.Intersects(rectangle2))
+				{
+					// Subtract the health from the player based on
+					// the enemy damage
+					player.Health -= enemies[i].Damage;
+
+					// Since the enemy collided with the player
+					// destroy it
+					enemies[i].Health = 0;
+
+					// If the player health is less than zero we died
+					if (player.Health <= 0)
+						player.Active = false;
+				}
+
+
+			}
+			// Projectile vs Enemy Collision
+			for (int i = 0; i < projectiles.Count; i++)
+			{
+				for (int j = 0; j < enemies.Count; j++)
+				{
+					// Create the rectangles we need to determine if we collided with each other
+					rectangle1 = new Rectangle((int)projectiles[i].Position.X -
+					projectiles[i].Width / 2, (int)projectiles[i].Position.Y -
+					projectiles[i].Height / 2, projectiles[i].Width, projectiles[i].Height);
+
+					rectangle2 = new Rectangle((int)enemies[j].Position.X - enemies[j].Width / 2,
+					(int)enemies[j].Position.Y - enemies[j].Height / 2,
+					enemies[j].Width, enemies[j].Height);
+
+					// Determine if the two objects collided with each other
+					if (rectangle1.Intersects(rectangle2))
+					{
+						enemies[j].Health -= projectiles[i].Damage;
+						projectiles[i].Active = false;
+					}
+				}
+			}
+		}
+		private void AddProjectile(Vector2 position)
+		{
+			Projectile projectile = new Projectile();
+			projectile.Initialize(GraphicsDevice.Viewport, projectileTexture, position);
+			projectiles.Add(projectile);
+		}
+		private void UpdateProjectiles()
+		{
+			// Update the Projectiles
+			for (int i = projectiles.Count - 1; i >= 0; i--)
+			{
+				projectiles[i].Update();
+
+				if (projectiles[i].Active == false)
+				{
+					projectiles.RemoveAt(i);
+				}
+			}
+		}
+
+		private void AddExplosion(Vector2 position)
+		{
+			Animation explosion = new Animation();
+			explosion.Initialize(explosionTexture, position, 134, 134, 12, 45, Color.White, 1f, false);
+			explosions.Add(explosion);
+		}
+		private void UpdateExplosions(GameTime gameTime)
+		{
+			for (int i = explosions.Count - 1; i >= 0; i--)
+			{
+				explosions[i].Update(gameTime);
+				if (explosions[i].Active == false)
+				{
+					explosions.RemoveAt(i);
+				}
+			}
+		}
+
+		private void PlayMusic(Song song)
+		{
+			// Due to the way the MediaPlayer plays music,
+			// we have to catch the exception. Music will play when the game is not tethered
+			try
+			{
+				// Play the music
+				MediaPlayer.Play(song);
+
+				// Loop the currently playing song
+				MediaPlayer.IsRepeating = true;
+			}
+			catch { }
+		}
 	}
 }
+
